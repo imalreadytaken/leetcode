965e47d6918f2c0a3fb59f32f43caa33699cd86d
imalreadytaken <csh7@tju.edu.cn>
2018-06-14 19:07:46 +0800

    4sum-timeout

--- src/medium/FourSum.java
+++ src/medium/FourSum.java
@@ -1,7 +1,10 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 /**
  * Given an array nums of n integers and an integer target,
@@ -26,17 +29,36 @@ import java.util.List;
 
 public class FourSum {
 
-    List<List<Integer>> result;
+
+    public static void main(String[] args) {
+        FourSum f = new FourSum();
+        int[] nums = {-5,5,4,-3,0,0,4,-2};
+        int target = 4;
+        System.out.println(f.fourSum(nums, target));
+    }
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        result = new ArrayList<>();
+        if(nums == null || nums.length < 4) return new ArrayList<>();
 
-        return result;
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = someSum(nums, target,0, 4);
+        return result == null ? new ArrayList<>() : result.stream().map((l)->{return l.stream().sorted().collect(Collectors.toList());}).distinct().collect(Collectors.toList());
     }
 
     private List<List<Integer>> someSum(int[] nums, int target, int index, int cnt){
-        if (nums.length - index < cnt){
+        if (cnt < 1 || nums.length - index < cnt){
             return null;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        if (cnt == 1){
+            for (int i = index; i < nums.length; i++) {
+                if (nums[i] == target){
+                    List<Integer> res = new ArrayList<Integer>();
+                    res.add(nums[i]);
+                    result.add(res);
+                }
+            }
         }
         if(nums.length - index == cnt){
             int sum = 0;
@@ -45,14 +67,21 @@ public class FourSum {
                 sum += nums[i];
                 res.add(nums[i]);
             }
-            if (sum == target) ;
+            if (sum == target) result.add(res);
+            return result;
         }
-        for ( List<Integer> l : someSum(nums, target-nums[index], index+1, cnt - 1) ) {
-            l.add(nums[index]);
-            result.add(l);
+        List<List<Integer>> tmp = someSum(nums, target-nums[index], index+1, cnt - 1);
+        if (tmp!=null){
+            for ( List<Integer> l : tmp ) {
+                l.add(nums[index]);
+                result.add(l);
+            }
         }
-        for (List<Integer> l: someSum(nums, target, index+1,cnt)) {
-            result.add(l);
+        tmp = someSum(nums, target, index+1,cnt);
+        if (tmp!=null){
+            for (List<Integer> l: someSum(nums, target, index+1,cnt)) {
+                result.add(l);
+            }
         }
         return result;
     }
