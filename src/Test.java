c5ecd213861814430e638664faef82620cb93897
imalreadytaken <csh7@tju.edu.cn>
2018-06-16 13:48:34 +0800

    99.06% Remove Element

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Test {
     /**
      * test
+     *
      * @param args stete
      */
     public static void main(String[] args) {
-        System.out.println("test");
+        List<List<Integer>> mul = new ArrayList<>();
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(0);
+        l.add(-1);
+        l.add(0);
+        l.add(1);
+        List<Integer> l2 = new ArrayList<Integer>();
+        l.add(0);
+        l.add(-1);
+        l.add(0);
+        l.add(1);
+        mul.add(l);
+
+        System.out.println(mul.contains(l2));
     }
 }
