b5807c673a5cb74144ae99a3c5d0d73549785046
imalreadytaken <csh7@tju.edu.cn>
2018-06-16 14:19:45 +0800

    not finished

---
+++ src/easy/CountAndSay.java
@@ -0,0 +1,56 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * The count-and-say sequence is the sequence of integers with the first five terms as following:
+ *
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 1 is read off as "one 1" or 11.
+ * 11 is read off as "two 1s" or 21.
+ * 21 is read off as "one 2, then one 1" or 1211.
+ * Given an integer n, generate the nth term of the count-and-say sequence.
+ *
+ * Note: Each term of the sequence of integers will be represented as a string.
+ *
+ * Example 1:
+ *
+ * Input: 1
+ * Output: "1"
+ * Example 2:
+ *
+ * Input: 4
+ * Output: "1211"
+ */
+public class CountAndSay {
+
+    private StringBuilder result;
+    private ArrayList<String> strs = new ArrayList<>();
+
+    public String countAndSay(int n) {
+        if (strs.size() >= n) return strs.get(n-1);
+        String current = strs.get(strs.size() - 1);
+        char tmp = current.charAt(0);
+        int times = 1;
+        while (strs.size() < n){
+            result = new StringBuilder();
+            for (int i = 1; i < current.length(); i++) {
+                if (tmp == current.charAt(i)) times++;
+                else {
+                    result.append(times);
+                    result.append(tmp);
+                    tmp = current.charAt(i);
+                    times = 1;
+                }
+            }
+            strs.add(result.toString());
+        }
+        return result.toString();
+    }
+
+}
