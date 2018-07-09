f0708991741a31d4893168f4edaee1da00579fc9
seven <csh7@tju.edu.cn>
2018-07-09 23:00:40 +0800

    93.9% Array Partition I

---
+++ src/easy/ArrayPartitionI.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+ *
+ * Example 1:
+ * Input: [1,4,3,2]
+ *
+ * Output: 4
+ * Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+ * Note:
+ * n is a positive integer, which is in the range of [1, 10000].
+ * All the integers in the array will be in the range of [-10000, 10000].
+ */
+public class ArrayPartitionI {
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0, i = 0;
+        while (i < nums.length) {
+            res+=nums[i];
+            i += 2;
+        }
+        return res;
+    }
+
+}
