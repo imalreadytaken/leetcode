d5f4f3643f813092623b6c35a77009833f836a5a
seven <csh7@tju.edu.cn>
2018-07-15 23:38:44 +0800

    emmm。。。the world cup. so...

--- src/easy/ArrayPartitionI.java
+++ src/easy/ArrayPartitionI.java
@@ -3,6 +3,7 @@ package easy;
 import java.util.Arrays;
 
 /**
+ * emmm, the world cup, so...
  * Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
  *
  * Example 1:
