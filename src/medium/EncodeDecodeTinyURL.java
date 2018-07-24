2508bc591c68d8f824e4fd7aad0181a941d52714
seven <csh7@tju.edu.cn>
2018-07-24 22:13:39 +0800

    100% Encode and Decode TinyURL this is a bad problem hhh.

---
+++ src/medium/EncodeDecodeTinyURL.java
@@ -0,0 +1,18 @@
+package medium;
+
+/**
+ * TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+ *
+ * Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+ */
+public class EncodeDecodeTinyURL {
+
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+
+}
