8a9e48f808129dc1b96b4cab699da03e9a9452b9
seven <csh7@tju.edu.cn>
2018-08-02 21:58:22 +0800

    0% LRU Cache

--- src/hard/LRUCache.java
+++ src/hard/LRUCache.java
@@ -1,5 +1,10 @@
 package hard;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
  *
@@ -27,19 +32,55 @@ public class LRUCache {
 
     private int capacity;
     private int size;
+    private Queue<Integer> que;
+    private Map<Integer, Integer> map;
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        size = 0;
+        this.size = 0;
+        this.que = new LinkedList<>();
+        this.map = new HashMap<>();
     }
 
     public int get(int key) {
         // 判断是否存在
-
-        // 返回，并调整队列
-        return 0;
+        if (map.containsKey(key)) {
+            // 调整队列, 并返回
+            if (que.contains(key)){
+                que.remove(Integer.valueOf(key));
+            }
+            que.offer(key);
+            return map.get(key);
+        }
+        return -1;
     }
 
     public void put(int key, int value) {
-
+        if (map.containsKey(key)){
+            map.put(key, value);
+        }else {
+            if (this.size >= this.capacity){
+                map.remove(que.poll());
+            }else size++;
+            map.put(key, value);
+        }
+        if (que.contains(key)){
+            que.remove(Integer.valueOf(key));
+        }
+        que.offer(key);
     }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+        cache.put(2, 1);
+        cache.put(1, 1);
+        cache.put(2, 3);    // evicts key 2
+        cache.put(4, 1);    // evicts key 1
+        cache.get(1);       // returns 1
+        cache.get(2);       // returns -1 (not found)
+//        cache.get(1);       // returns -1 (not found)
+//        cache.get(3);       // returns 3
+//        cache.get(4);       // returns 4
+    }
+
 }
