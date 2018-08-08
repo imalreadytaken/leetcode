bc73dcaedeb009c486c4ef6dc7a688cf4e01797b
seven <csh7@tju.edu.cn>
2018-08-08 22:21:19 +0800

    todo

---
+++ src/medium/UniqueBinarySearchTreesII.java
@@ -0,0 +1,47 @@
+package medium;
+
+import java.util.List;
+
+/**
+ * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+ *
+ * Example:
+ *
+ * Input: 3
+ * Output:
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ */
+public class UniqueBinarySearchTreesII {
+
+//    private List<TreeNode> generateTree(int start, int end){
+//        if (start == end) return new TreeNode(start);
+//    }
+
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
