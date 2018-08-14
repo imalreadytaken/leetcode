3115a7923cd1472c136ce58fad2200f57bf14fa7
seven <csh7@tju.edu.cn>
2018-08-14 22:28:48 +0800

    78.33% Loud and Rich

---
+++ src/medium/LoudAndRich.java
@@ -0,0 +1,84 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * In a group of N people (labelled 0, 1, 2, ..., N-1), each person has different amounts of money, and different levels of quietness.
+ *
+ * For convenience, we'll call the person with label x, simply "person x".
+ *
+ * We'll say that richer[i] = [x, y] if person x definitely has more money than person y.  Note that richer may only be a subset of valid observations.
+ *
+ * Also, we'll say quiet[x] = q if person x has quietness q.
+ *
+ * Now, return answer, where answer[x] = y if y is the least quiet person (that is, the person y with the smallest value of quiet[y]), among all people who definitely have equal to or more money than person x.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: richer = [[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]], quiet = [3,2,5,4,6,1,7,0]
+ * Output: [5,5,2,5,4,5,6,7]
+ * Explanation:
+ * answer[0] = 5.
+ * Person 5 has more money than 3, which has more money than 1, which has more money than 0.
+ * The only person who is quieter (has lower quiet[x]) is person 7, but
+ * it isn't clear if they have more money than person 0.
+ *
+ * answer[7] = 7.
+ * Among all people that definitely have equal to or more money than person 7
+ * (which could be persons 3, 4, 5, 6, or 7), the person who is the quietest (has lower quiet[x])
+ * is person 7.
+ *
+ * The other answers can be filled out with similar reasoning.
+ * Note:
+ *
+ * 1 <= quiet.length = N <= 500
+ * 0 <= quiet[i] < N, all quiet[i] are different.
+ * 0 <= richer.length <= N * (N-1) / 2
+ * 0 <= richer[i][j] < N
+ * richer[i][0] != richer[i][1]
+ * richer[i]'s are all different.
+ * The observations in richer are all logically consistent.
+ */
+public class LoudAndRich {
+
+    public int[] loudAndRich(int[][] richer, int[] quiet) {
+        int n = quiet.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) res[i] = -1;
+        List<Integer>[] biggers = new List[n];
+        for (int i = 0; i < richer.length; i++) {
+            if (biggers[richer[i][1]] == null) biggers[richer[i][1]] = new ArrayList();
+            biggers[richer[i][1]].add(richer[i][0]);
+        }
+        for (int i = 0; i < n; i++) {
+            getOneResult(i, quiet, biggers, res);
+        }
+        return res;
+    }
+    private int getOneResult(int n, int[] quiet, List<Integer>[] biggers, int[] res){
+        if (res[n] >= 0) return res[n];
+        int quietness = quiet[n], result = n;
+        if (biggers[n] != null){
+            // 递归去找比他rich的人里面比他quiet的人
+            for (int bigger: biggers[n]) {
+                int tmp = getOneResult(bigger, quiet, biggers, res);
+                if (quietness > quiet[tmp]){
+                    quietness = quiet[tmp];
+                    result = tmp;
+                }
+            }
+        }
+        res[n] = result;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] quiet = {3,2,5,4,6,1,7,0};
+        int[][] richer = {{1,0},{2,1},{3,1},{3,7},{4,3},{5,3},{6,3}};
+        new LoudAndRich().loudAndRich(richer, quiet);
+    }
+
+}
