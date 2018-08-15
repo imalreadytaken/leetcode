cfbea019ceb52e2f3efdf1be31d09cee52bc0dc5
seven <csh7@tju.edu.cn>
2018-08-15 23:04:00 +0800

    99.63% Couples Holding Hands

---
+++ src/hard/CouplesHoldingHands.java
@@ -0,0 +1,47 @@
+package hard;
+
+/**
+ * N couples sit in 2N seats arranged in a row and want to hold hands. We want to know the minimum number of swaps so that every couple is sitting side by side. A swap consists of choosing any two people, then they stand up and switch seats.
+ *
+ * The people and seats are represented by an integer from 0 to 2N-1, the couples are numbered in order, the first couple being (0, 1), the second couple being (2, 3), and so on with the last couple being (2N-2, 2N-1).
+ *
+ * The couples' initial seating is given by row[i] being the value of the person who is initially sitting in the i-th seat.
+ *
+ * Example 1:
+ *
+ * Input: row = [0, 2, 1, 3]
+ * Output: 1
+ * Explanation: We only need to swap the second (row[1]) and third (row[2]) person.
+ * Example 2:
+ *
+ * Input: row = [3, 2, 0, 1]
+ * Output: 0
+ * Explanation: All couples are already seated side by side.
+ * Note:
+ *
+ * len(row) is even and in the range of [4, 60].
+ * row is guaranteed to be a permutation of 0...len(row)-1.
+ */
+public class CouplesHoldingHands {
+    public int minSwapsCouples(int[] row) {
+        int n = row.length;
+        // 记录位置
+        int[] pos = new int[n];
+        for (int i = 0; i < n; i++) {
+            pos[row[i]] = i;
+        }
+        int swap = 0, left, dest;
+        // 开始交换
+        for (int i = 0; i < n; i+=2) {
+            left = row[i] ^ 1;
+            if (row[i+1] != left){
+                // 交换row中第i+1和第pos[ row[i]^1 ]个数
+                dest = pos[left];
+                pos[row[i+1]] = dest;
+                row[dest] = row[i+1];
+                swap++;
+            }
+        }
+        return swap;
+    }
+}
