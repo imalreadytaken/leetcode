a62d965713403ba92dcbf1b1f3a36095e67d85b0
seven <csh7@tju.edu.cn>
2018-09-23 22:57:02 +0800

    sort list // todo

---
+++ src/medium/SortList.java
@@ -0,0 +1,39 @@
+package medium;
+
+import java.util.List;
+
+/**
+ * 归并排序，todo
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 4->2->1->3
+ * Output: 1->2->3->4
+ * Example 2:
+ * <p>
+ * Input: -1->5->3->4->0
+ * Output: -1->0->3->4->5
+ */
+public class SortList {
+
+    public ListNode sortList(ListNode head) {
+        System.out.println();
+        return null;
+    }
+
+    private ListNode[] merge(){
+        return null;
+    }
+
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
