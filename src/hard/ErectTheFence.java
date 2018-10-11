4c1df21649bda28583e20015a509e98e51f1e10e
seven <csh7@tju.edu.cn>
2018-10-11 21:14:49 +0800

    4% Erect the Fence

--- src/hard/ErectTheFence.java
+++ src/hard/ErectTheFence.java
@@ -3,6 +3,8 @@ package hard;
 import java.util.*;
 
 /**
+ * 先从左到右排序，
+ * 然后从左到右找到下半环，再从右到左找到上半环
  * There are some trees, where each tree is represented by (x,y) coordinate in a two-dimensional garden. Your job is to fence the entire garden using the minimum length of rope as it is expensive. The garden is well fenced only if all the trees are enclosed. Your task is to help find the coordinates of trees which are exactly located on the fence perimeter.
  * <p>
  * Example 1:
